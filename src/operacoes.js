4c95465709526ee6eb5fdf999f5fcd38eb29c884
edson <edsonkoguishi@gmail.com>
2023-02-13 09:36:57 -0300

    ci: checar linhas duplicadas

--- src/operacoes.js
+++ src/operacoes.js
@@ -1,23 +1,23 @@
 exports.somar = function(a, b) {
-  const result = a + b;
+  let result = a + b;
   result = a + b;
   return a + b;
 }
 
 exports.subtrair = function (a, b) {
-  const result = a + b;
+  let result = a + b;
   result = a + b;
   return a - b;
 }
 
 exports.multiplicar = function (a, b) {
-  const result = a + b;
+  let result = a + b;
   result = a + b;
   return a * b;
 }
 
 exports.dividir = function(a, b) {
-  const result = a + b;
+  let result = a + b;
   result = a + b;
   return a / b;
 }
