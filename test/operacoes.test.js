6b0f7a80309c876eff6a75e8470643b0c5b9acdb
edson <edsonkoguishi@gmail.com>
2023-02-13 09:26:56 -0300

    test: aumentar cobertura de testes

--- test/operacoes.test.js
+++ test/operacoes.test.js
@@ -1,5 +1,17 @@
 const op = require('../src/operacoes');
 
-test('mult 5 * 5 to equal 25', () => {
+test('5 + 5 = 10', () => {
+  expect(op.somar(5, 5)).toBe(10);
+});
+
+test('5 - 5 = 0', () => {
+  expect(op.subtrair(5, 5)).toBe(0);
+});
+
+test('5 * 5 = 25', () => {
   expect(op.multiplicar(5, 5)).toBe(25);
+});
+
+test('5 / 5 = 1', () => {
+  expect(op.dividir(5, 5)).toBe(1);
 });
