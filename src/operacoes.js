0a1a3ce3829df32bb05dbff81f185af0c76df478
edson <edsonkoguishi@gmail.com>
2023-02-13 09:45:50 -0300

    ci: retirar linhas duplicadas

--- src/operacoes.js
+++ src/operacoes.js
@@ -1,29 +1,15 @@
 exports.somar = function(a, b) {
-  let result = a + b;
-  result = a + b;
-  result = a + b;
   return a + b;
 }
 
 exports.subtrair = function (a, b) {
-  let result = a + b;
-  result = a + b;
-  result = a + b;
   return a - b;
 }
 
 exports.multiplicar = function (a, b) {
-  let result = a + b;
-  result = a + b;
-  result = a + b;
   return a * b;
 }
 
 exports.dividir = function(a, b) {
-  let result = a + b;
-  result = a + b;
-  result = a + b;
   return a / b;
 }
-
-// module.exports = multiplicar;
